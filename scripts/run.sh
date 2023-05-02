97114cfddc5e2cc33ec9fd15bbd8085354a6f32a
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2023-05-01 23:34:12 -0400

    Add dev setup scripts

---
+++ scripts/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh 
+# Script run to finish building the app container 
+
+set -e 
+
+# run commands needed to start server
+python manage.py wait_for_db
+# collect static files from all Django apps into STATIC_ROOT (--noinput prevents prompt)
+python manage.py collectstatic --noinput
+
+# run on socket on port 9000 (nginx serves 9000)
+# workers: # of workers working concurrently in the container accepting requests
+# run as master daemon (foreground)
+# enable multithreading
+# run as wsgi service
+uwsgi --socket :9000 --workers 1 --master --enable-threads --module barcode_identifier_api.wsgi
