3fd155b93c8cc7a9f5e63e9fea79b23861179c65
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2023-06-22 11:18:32 -0400

    Change auth class settings

--- barcode_identifier_api/settings.py
+++ barcode_identifier_api/settings.py
@@ -63,6 +63,7 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'knox.auth.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ),
 }
 
