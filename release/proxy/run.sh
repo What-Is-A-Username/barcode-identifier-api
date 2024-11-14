2ed6f3c680a7f9804301054c3a998ea496153b69
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2024-11-14 15:31:51 -0500

    Update README, remove scripts

--- release/proxy/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# 
-set -e
-
-# move config into default.conf while substituting in ENV variables
-envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
-# start in nginx in the foreground, not the background, so debug is sent to log
-nginx -g 'daemon off;'
