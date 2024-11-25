5ae9ee34196de191a3f82b28b23458efa967d1bb
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2024-11-25 18:00:19 -0500

    Change version parsing of custom seq

--- barcode_blastn/controllers/custom_sequence_controller.py
+++ barcode_blastn/controllers/custom_sequence_controller.py
@@ -18,10 +18,9 @@ def parse_file_upload_to_custom_sequence(upload: UploadedFile, database: BlastDb
     data = [line.decode('utf-8') for line in upload.readlines()]
     for record in SeqIO.parse(StringIO('\n'.join(data)), "fasta"):
         accession, definition = record.description.split(' ', 1)
+        version = accession
         if '.' in accession:
-            accession, version = accession.rsplit('.', 1)
-        else:
-            version = accession
+            accession = accession.rsplit('.', 1)[0]
         seqs.append(
             CustomSequence(
                 dna_sequence=str(record.seq),
